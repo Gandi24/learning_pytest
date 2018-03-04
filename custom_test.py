d1a4916fc4acb962f62bae904a6bfa19863b03d2
Gandi24 <kuba.wasielak@gmail.com>
2018-03-05 00:41:37 +0100

    2.14 Pytest mark

--- custom_test.py
+++ custom_test.py
@@ -1,7 +1,13 @@
+import os
+import pytest
+
+
+@pytest.mark.skipif(os.environ.get('NO_SUMMING') == '1', reason='NO_SUMMING set to 1')
 def test_sum():
     assert 2+2 == 4
 
 
+@pytest.mark.xfail
 def test_get_element_from_list():
     custom_list = ["test"]
-    assert custom_list[0] == "test"
+    assert custom_list[0] == "fail"
