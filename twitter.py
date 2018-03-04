c09d2fb356d98f3dbf764ea557a644f18a244336
Gandi24 <kuba.wasielak@gmail.com>
2018-03-04 12:06:07 +0100

    2.6 Requests

--- twitter.py
+++ twitter.py
@@ -1,25 +1,46 @@
+import json
 import re
+from urllib.parse import urljoin
+
+import requests
+
+USERS_API = "https://api.github.com/users/"
 
 
 class Twitter(object):
     version = '1.0'
 
-    def __init__(self, backend=None):
+    def __init__(self, backend=None, username=None):
         self.backend = backend
         self._tweets = []
+        self.username = username
 
     @property
     def tweets(self):
         if self.backend and not self._tweets:
-            self._tweets = [line.rstrip('\n') for line in self.backend.readlines()]
+            backend_text = self.backend.read()
+            if backend_text:
+                self._tweets = json.loads(backend_text)
         return self._tweets
+
+    @property
+    def tweet_messages(self):
+        return [tweet['message'] for tweet in self.tweets]
+
+    def get_user_avatar(self):
+        if not self.username:
+            return None
+
+        url = urljoin(USERS_API, self.username)
+        resp = requests.get(url)
+        return resp.json()['avatar_url']
 
     def tweet(self, message):
         if len(message) > 160:
             raise Exception("Message too long.")
-        self.tweets.append(message)
+        self.tweets.append({'message': message, 'avatar': self.get_user_avatar()})
         if self.backend:
-            self.backend.write("\n".join(self.tweets))
+            self.backend.write(json.dumps(self.tweets))
 
     def find_hashtags(self, message):
         return [m.lower() for m in re.findall("#(\w+)", message)]
--- twitter_test.py
+++ twitter_test.py
@@ -25,13 +25,13 @@ def test_twitter_initialization(twitter):
 
 def test_tweet_single_message(twitter):
     twitter.tweet('Test message')
-    assert twitter.tweets == ['Test message']
+    assert twitter.tweet_messages == ['Test message']
 
 
 def test_tweet_long_message(twitter):
     with pytest.raises(Exception):
         twitter.tweet('test'*41)
-    assert twitter.tweets == []
+    assert twitter.tweet_messages == []
 
 
 def test_initialize_two_twitter_classes(backend):
@@ -41,7 +41,7 @@ def test_initialize_two_twitter_classes(backend):
     twitter1.tweet('Test 1')
     twitter1.tweet('Test 2')
 
-    assert twitter2.tweets == ['Test 1', 'Test 2']
+    assert twitter2.tweet_messages == ['Test 1', 'Test 2']
 
 
 @pytest.mark.parametrize("message, expected", (
